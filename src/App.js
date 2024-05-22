c00e027cd1fde35c11c913d35126e0aea8027c82
sunil <sunildhinagaran@gmail.com>
2024-05-22 22:02:30 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HeadlineCards from './components/HeadlineCards'
+import Food from './components/Food'
+import Category from './components/Category'
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <Navbar />
     <Hero />
     <HeadlineCards />
+    <Food />
+    <Category />
     </div>
   );
 }
